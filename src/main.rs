a26007d1ab547d8471f287fe27c8de05062790a0
makisat <makiya28sato@gmail.com>
2024-05-01 20:41:50 -0400

    display distro name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use text_to_ascii_art::convert;
+
 fn main() {
-    println!("Hello, world!");
+    let content = std::fs::read_to_string("/etc/os-release").expect("Failed to read file");
+
+    let lines: Vec<&str> = content.split("\n").collect();
+
+    for line in lines {
+        if line.starts_with("NAME=") {
+            let name = line.split("=").collect::<Vec<&str>>()[1];
+            let name = name.replace("\"", "");
+
+            let ascii_art = convert(name.to_owned()).unwrap();
+            println!("{}", ascii_art);
+        }
+    }
 }
